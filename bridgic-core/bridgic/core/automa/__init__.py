05b50a2e04c011c0ebf007923b8776ed7753e1a7
wulinjiang <>
2025-09-19 11:44:28 +0800

    change module name parameter_inject -> arguments_inject

--- bridgic-core/bridgic/core/automa/__init__.py
+++ bridgic-core/bridgic/core/automa/__init__.py
@@ -2,7 +2,7 @@ from bridgic.core.automa.automa import Automa, GoalOrientedAutoma
 from bridgic.core.automa.graph_automa import GraphAutoma
 from bridgic.core.automa.goap_automa import GoapAutoma
 from bridgic.core.automa.llmp_automa import LlmpAutoma, PlanningStrategy
-from bridgic.core.automa.arguments_inject import From, System
+from bridgic.core.automa.arguments_inject import From
 from bridgic.core.automa.worker_decorator import worker, StaticOutputEffect, DynamicOutputEffect, ArgsMappingRule
 from bridgic.core.automa.goal_decorator import goal
 from bridgic.core.types.error import *
