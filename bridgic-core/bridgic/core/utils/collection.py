77d737d840ad671c87c5cb26e51201125fc99800
tsingfei <tsingfei@gmail.com>
2025-10-08 15:33:43 +0800

    Refactor: use mark_overload to classify worker decorator. (#30)
    * Docs: modify documentation of event handling.
    
    * Refactor: move event handling and human interaction up to Automa class.
    
    * Refactor: move loacl_space related interface up to Automa.
    
    * Refactor: use mark_overload and get_mark_by_func to classify different worker overloads.

--- bridgic-core/bridgic/core/utils/collection.py
+++ bridgic-core/bridgic/core/utils/collection.py
@@ -1,4 +1,5 @@
 from typing import List, Dict, Any
+from collections.abc import Hashable
 
 def unique_list_in_order(ele_list: List[Any]) -> List[Any]:
     """
@@ -11,3 +12,20 @@ def unique_list_in_order(ele_list: List[Any]) -> List[Any]:
             seen.add(ele)
             unique_ele.append(ele)
     return unique_ele
+
+def deep_hash(obj) -> int:
+    """
+    Recursively convert an object to a hashable form and calculate the hash value.
+    """
+    if isinstance(obj, (str, int, float, bool, type(None))):
+        return hash(obj)
+    elif isinstance(obj, (tuple, list)):
+        return hash(tuple(deep_hash(e) for e in obj))
+    elif isinstance(obj, dict):
+        return hash(tuple(sorted((deep_hash(k), deep_hash(v)) for k, v in obj.items())))
+    elif isinstance(obj, set):
+        return hash(tuple(sorted(deep_hash(e) for e in obj)))
+    elif isinstance(obj, Hashable):
+        return hash(obj)
+    else:
+        raise TypeError(f"Unhashable type: {type(obj)}")
